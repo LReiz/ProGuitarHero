780d0d80b9b56434f27c2ed8d59a8d06581b7719
LReiz <leoreis.lar@gmail.com>
2020-10-25 22:03:24 -0300

    filtragem de volumes muito baixos

--- audio_processor.py
+++ audio_processor.py
@@ -2,7 +2,7 @@ from config import (
 	INPUT_SAMPLE_RATE,
 )
 
-from notes_tools import find_note_id, find_note_string
+from notes_tools import find_note_id, find_note_string, volume_too_low
 from frequency_extractor import freq_from_autocorr
 from audio_player import play_music
 import numpy as np
@@ -10,8 +10,12 @@ import numpy as np
 def process_audio_forever(audioRecorder):
 	while(True):
 		frame = audioRecorder.getFrame()
-		frameFrequency = freq_from_autocorr(np.fromstring(frame, np.int16), INPUT_SAMPLE_RATE)
 
-		noteString = find_note_string(find_note_id(frameFrequency)) 
+		amplitudeArray = np.fromstring(frame, np.int16)
 
-		play_music(noteString)
+		if not volume_too_low(amplitudeArray):
+			frameFrequency = freq_from_autocorr(amplitudeArray, INPUT_SAMPLE_RATE)
+			print(max(amplitudeArray))
+			noteString = find_note_string(find_note_id(frameFrequency)) 
+
+			play_music(noteString)
--- notes_tools.py
+++ notes_tools.py
@@ -1,18 +1,27 @@
 from config import (
 	MIN_NOTE_FREQUENCY,
 	MIN_NOTE_ID,
-	BASE_NOTES_ARRAY
+	BASE_NOTES_ARRAY,
+	MIN_VOLUME
 )
 
 from math import (
-	log
+	log,
 )
 
 def find_note_id(frequency):
 	return round(log( frequency/MIN_NOTE_FREQUENCY , 2**(1/12) ) + MIN_NOTE_ID)
+
 
 def find_note_string(id):
 	baseNote = BASE_NOTES_ARRAY[id % 12]
 	noteFamily = int(id/12)
 
 	return (str(baseNote) + str(noteFamily))
+
+
+def volume_too_low(sig):
+	if max(sig) < abs(MIN_VOLUME):
+		return True
+
+	return False
